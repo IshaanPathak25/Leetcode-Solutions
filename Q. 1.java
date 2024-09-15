df670729b14d703b538b6d756790bd96b251221f
Ishaan Pathak <108518254+IshaanPathak25@users.noreply.github.com>
2024-09-15 21:33:24 +0530

    Update Q. 1.java

--- Q. 1.java
+++ Q. 1.java
@@ -12,3 +12,19 @@ class Solution {
         return null;
     }
 }
+
+
+//Another solution
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+            for (int i = 1; i < nums.length; i++) {
+                for (int j = i; j< nums.length; j++) {
+                    if (nums[j-i] + nums[j] == target) {
+                        return new int[] {j-i,j};
+                    }
+                }
+            }
+        return new int[]{};
+    }
+}
