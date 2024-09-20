78a92b8b4fb9629c98193157e0d8e311dde7559d
Ishaan Pathak <108518254+IshaanPathak25@users.noreply.github.com>
2024-09-20 16:46:22 +0530

    Create Q. 179.java

---
+++ Q. 179.java
@@ -0,0 +1,90 @@
+class Solution {
+    public void merge(int[]arr,int low,int mid,int high)
+    {
+        int i=low;int j=mid+1;
+        int[]temp=new int[high-low+1];
+        int k=0;
+        while(i<=mid && j<=high)
+        {
+            long n=10,m=10;
+            while(arr[i]>=n)
+            {
+                n*=10;
+            }
+            while(arr[j]>=m)
+            {
+                m*=10;
+            }
+            long l=arr[i]*m+arr[j];
+            long r=arr[j]*n+arr[i];
+            if(l>r)
+            {
+              temp[k++]=arr[i++];
+            }
+            else
+            {
+                temp[k++]=arr[j++];
+            }
+        }
+        while(i<=mid)
+        {
+            temp[k++]=arr[i++];
+
+        }
+        while(j<=high)
+        {
+
+            temp[k++]=arr[j++];
+        }
+        k=0;
+        int m=low;
+        while(m<=high)
+        {
+            arr[m++]=temp[k++];
+        }
+    }
+    public void mergesort(int[]arr,int low,int high)
+    {
+        if(low==high)
+        {
+            return ;
+        }
+        int mid=low+(high-low)/2;
+        mergesort(arr,low,mid);
+        mergesort(arr,mid+1,high);
+        merge(arr,low,mid,high);
+    }
+    public String largestNumber(int[] nums) {
+       mergesort(nums,0,nums.length-1);
+       StringBuilder str=new StringBuilder();
+       for(int i=0;i<nums.length;i++)
+       {
+         str.append(String.valueOf(nums[i]));
+       }
+       return (str.charAt(0)=='0')?"0":str.toString();
+    }
+}
+
+
+    // public String largestNumber(int[] nums) {
+    //      String[] numStrs = new String[nums.length];
+    //     for (int i = 0; i < nums.length; i++) {
+    //         numStrs[i] = String.valueOf(nums[i]);
+    //     }
+
+    //     // Sort the strings based on custom comparator
+    //     Arrays.sort(numStrs, (a, b) -> (b + a).compareTo(a + b));
+
+    //     // Edge case: if the largest number is 0, return "0"
+    //     if (numStrs[0].equals("0")) {
+    //         return "0";
+    //     }
+
+    //     // Concatenate the sorted array to form the largest number
+    //     StringBuilder result = new StringBuilder();
+    //     for (String numStr : numStrs) {
+    //         result.append(numStr);
+    //     }
+
+    //     return result.toString();
+    // }
