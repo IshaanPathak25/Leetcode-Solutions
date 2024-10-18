3f9bf69440e4f3c302f2a1b6c372b80222a39fa0
Ishaan Pathak <108518254+IshaanPathak25@users.noreply.github.com>
2024-10-18 16:28:49 +0530

    Create Q. 495.java

---
+++ Q. 495.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int total = 0;
+        int startTime = timeSeries[0];
+        int endTime = startTime + duration;
+        for (int i = 1; i < timeSeries.length; i++) {
+            int time = timeSeries[i];
+            if (time > endTime) {
+                total += endTime - startTime;
+                startTime = time;
+            }
+            endTime = time + duration;
+        }
+        return total + (endTime - startTime);
+    }
+}
+
+
+//MORE OPTIMISED SOLUTION BELOW
+
+//class Solution {
+//    public int findPoisonedDuration(int[] timeSeries, int duration) {
+//        int total = 0;
+//        int startTime = timeSeries[0];
+//        int endTime = startTime + duration;
+//        for (int i = 1; i < timeSeries.length; i++) {
+//            int time = timeSeries[i];
+//            if (time > endTime) {
+//                total += endTime - startTime;
+//                startTime = time;
+//            }
+//            endTime = time + duration;
+//        }
+//        return total + (endTime - startTime);
+//    }
+//}
